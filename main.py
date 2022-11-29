b12ccf5e5b2a0e9e4672832094837ae8fd1b660c
Rushikesh Atole <>
2022-11-29 14:28:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello guys I am Rushikesh Atole")
+print("November 29th 2022")
+print("""I am signing up for Replit's 100 days of python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day. 
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be.No excuses for not coding from the middle of a field!
+""")
+print("I am feeling 😊")
+print("You can follow my progress at replit.com/@RushikeshAtole1")
